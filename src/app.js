a5ded45e43dce8ac86fe02193ea7069c4b79bc50
NAnkita24 <apjagtap24@gmail.com>
2024-04-14 18:50:36 +0530

    14 April

--- src/app.js
+++ src/app.js
@@ -1,19 +1,46 @@
-const express=require('express')
-const app=express()
-const hbs=require('hbs')
-const mongoose=require('mongoose')
-const routes=require('./routes/main')
+const express = require('express')
+const routes = require('./routes/main')
+const hbs = require('hbs')
+const mongoose = require('mongoose')
+const Detail = require('./models/Detail')
+const app = express()
 
 //static and route
-app.use('/static',express.static('public'))
-app.use('',routes)
+app.use('/static', express.static('public'))
+app.use('', routes)
 
 //view engine
-app.set('view engine','hbs')
-app.set('views','views')
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 hbs.registerPartials('views/partials')
 
 //db connection
 mongoose.connect('mongodb://127.0.0.1:27017/servicehub')
-
+// Detail.create({
+//     brandName: "serviceHub",
+//     brandUrl: 'static/images/logo.png',
+//     links : [
+//         {
+//             label: 'Home',
+//             url: '/'
+//         },
+//         {
+//             label: 'Service',
+//             url: '/services'
+//         },
+//         {
+//             label: 'Gallery',
+//             url: '/gallery'
+//         },
+//         {
+//             label: 'About Us',
+//             url: '/about'
+//         },
+//         {
+//             label: 'Contact Us',
+//             url: '/contact'
+//         }
+        
+//     ]
+// })
 app.listen(5000)
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,9 +1,14 @@
 const express=require('express')
 const {route}=require('express/lib/application')
 const routes=express.Router()
+const Detail=require('../models/Detail')
 
-routes.get('/',(req,resp)=>{
-    resp.render("index")
+
+routes.get('/',async (req,resp)=>{
+    const details = await Detail.findOne({'_id':"661b7d04de8ef4d4d74d01a4"})
+    resp.render("index",{
+        details : details
+    })
 })
 
 routes.get('/gallery',(req,resp)=>{
